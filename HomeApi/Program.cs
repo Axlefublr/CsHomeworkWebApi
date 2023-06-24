101d1d9b0b3c6dc35bf0dc01c32bee468ad4b35b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-24 14:50:44 +0800

    convert indentation to tabs

--- HomeApi/Program.cs
+++ HomeApi/Program.cs
@@ -9,18 +9,19 @@ using Microsoft.Extensions.Logging;
 
 namespace HomeApi
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("https://localhost:5001/swagger/index.html");
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
